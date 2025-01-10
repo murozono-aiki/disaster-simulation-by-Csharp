f2eaf2f86a43447e809202839a4226ef923575e5
murozono-aiki <151797695+murozono-aiki@users.noreply.github.com>
2025-01-10 23:09:35 +0900

    Second Commit

---
+++ DisasterSimulation/Simulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisasterSimulation
+{
+    internal class Simulator
+    {
+    }
+
+    internal class PointData
+    {
+        public int index;
+        public Vector3[] triangle = new Vector3[3];
+        public Vector3 centerOfGravity;
+        public Vector3 normalVector;
+        public InsideJudge[] insideJudge;
+        public class InsideJudge
+        {
+            public Vector3 point;
+            public Vector3 normalVector;
+        }
+    }
+}
